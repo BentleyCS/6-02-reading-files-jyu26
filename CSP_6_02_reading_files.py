04f45f75e1619c72715a1f211f3640672c276a50
jyu26 <jyu26@bentleyschool.org>
2026-02-25 00:15:31 -0800

    Add files via upload

--- CSP_6_02_reading_files.py
+++ CSP_6_02_reading_files.py
@@ -8,13 +8,34 @@ def toString(fileName):
     for line in f:
         out += line
     return out
+
 #print(toString("ExampleText.txt")=="Here is the text\ni am another line")
+print(toString("ExampleText.txt"))
 
 def longestLine(fileName):
-    #Given a file return the longest line from within that file
-    pass
+    f = open(fileName)
+    longest = 0
+    answerLine = ""
+    for line in f:
+        x = len(line)
+        if x > longest:
+            longest = x
+            answerLine = line
+    return(answerLine)
+
 
 def toBinary(fileName):
     #Given a file that is only 0's and 1's return a list of the file broken into bytes.
     #An example return might be ['01101001', '00101010', '1010']
-    pass
+    f = open(fileName)
+    bytes = []
+
+    for line in f:
+        line = line.strip()
+        for b in range(0, int(len(line/8)):
+            bytes.append(line[b*8: (b+1)*8])
+
+        if len(line) - (b+1)*8 > 0:
+            bytes.append(line[(b+1)*8 : len(line) ])
+
+    return bytes
